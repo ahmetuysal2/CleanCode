75911b3564d84f117ddbf2c4685ee4e102f82b83
ahmetu <ahmet.uysal@paynet.com.tr>
2023-03-15 00:45:02 +0300

    Initial Commit

--- LiskovSubstitution/LiskovSubstitution/LiskovSubstitution/Program.cs
+++ LiskovSubstitution/LiskovSubstitution/LiskovSubstitution/Program.cs
@@ -7,12 +7,34 @@ public class Class1
 {
 
 
+    /*
+     * Lİskov Substitionda üst sınıfı kullanan alt sınıflar üst sınıfa ait özellikleri tamamen kullanabilmeli,anlamsal olarak bütüncül bir şekilde 
+     * kullanıp, hiç bir alt sınıfta üst sınıfa ait kullanılmayan bir özellik es geçimemeli (throw new ImplementedExecepiton) gibi
+     * Eğer böyle bir durum oluyor ise de bu duruma sebep olanb özellik için ayrı bir interface yaratılamlıdır.
+     * */
+
+    public static void Main(string[] args)
+    {
+        Duck duck = new MallardDuck();
+        duck.Fly();
+        /*
+         * POLYMORPHISM
+          Çok biçimlilik olarak bilinir.  Teknik olarak; bir üst sınıf referansının tüm alt sınıf nesnelerini tutabilmesidir.
+          Bilindiği gibi, alt sınıftaki nesneler üst ıfın sahip olduğu metotlara(static ve final tanımlı metotlar hariç) da sahiptirler 
+         (Örneğin, oluşturduğunuz bir nesne üzerinde Object sınıfına ait metotları görmeniz mümkündür).
+         Bu özellik ile birlikte Polymorphism, bir üst sınıf referansı ile alt sınıftaki nesnelerin kullanılabilmesine olanak sağlar. 
+         Buradaki avantaj, bir işlemi gerçekleştirirken hangi sınıfa ait nesne ile işlem gerçekleştirdiğimizi bilmemize gerek kalmamasıdır.
+
+         */
+    }
+
     public abstract class Duck
     {
         public abstract void Quack(); //All duck can quack
         public abstract void Swim(); // All duck can swim
         public abstract void Fly(); // But not all duck can swim for example  RubberDuck. So Ist not appropriate for Liskov Substitution Principle
                                     // We can use interface for the propery that subclass can not use
+
     }
 
 
@@ -31,19 +53,19 @@ public class Class1
         void Quack();
     }
 
-    public class MallardDuck : IFly, ISwim, IQuack
+    public class MallardDuck : Duck, IFly, ISwim, IQuack
     {
-        public void Quack()
+        public override void Quack()
         {
             System.Console.WriteLine("Quack!");
         }
 
-        public void Swim()
+        public override void Swim()
         {
             System.Console.WriteLine("Swimming!");
         }
 
-        public void Fly()
+        public override void Fly()
         {
             System.Console.WriteLine("Flying!");
         }
